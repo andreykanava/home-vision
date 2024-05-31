71422f3ad442a72b49ed8199bd872c7b5a89a54d
Andrew_0464 <kanava.andrey23@gmail.com>
2024-05-31 22:45:27 +0300

    .

--- screens/NetworkManager/main_screen.py
+++ screens/NetworkManager/main_screen.py
@@ -10,6 +10,7 @@ import asyncio
 import subprocess
 from curses import KEY_ENTER
 import threading
+import RPi.GPIO as GPIO
 
 def telegram_send_message(message):
     bot = telebot.TeleBot(TOKEN)
@@ -22,12 +23,55 @@ def main_network(stdscr):
     from screens.start_menu import main_screen
     from screens.schedule.main_screen import main_schedule
 
+    L1 = 5
+    L2 = 6
+    L3 = 13
+    L4 = 19
+
+    C1 = 12
+    C2 = 16
+    C3 = 20
+    C4 = 21
+
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+
+    GPIO.setup(L1, GPIO.OUT)
+    GPIO.setup(L2, GPIO.OUT)
+    GPIO.setup(L3, GPIO.OUT)
+    GPIO.setup(L4, GPIO.OUT)
+
+    GPIO.setup(C1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(C2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(C3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(C4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+    #            ^` ^l      ^o  ^e ^`           ^o  ^a   ^a ^b   ^o     ^o             
+    key_states = {
+        '1': False, '2': False, '3': False, 'A': False,
+        '4': False, '5': False, '6': False, 'B': False,
+        '7': False, '8': False, '9': False, 'C': False,
+        '*': False, '0': False, '#': False, 'D': False
+    }
+    def readLine(line, characters):
+        pressed_key = None
+        GPIO.output(line, GPIO.HIGH)
+        for i, char in enumerate(characters):
+            if GPIO.input([C1, C2, C3, C4][i]) == 1:
+                if not key_states[char]:
+                    pressed_key = char
+                    key_states[char] = True
+            else:
+                key_states[char] = False
+        GPIO.output(line, GPIO.LOW)
+        return pressed_key
+    time.sleep(0.3)
     while True:
         stdscr.clear()
         stdscr.nodelay(True)
         text_ui.add_text(stdscr, align.center_vertical, f'Enter network password', row=-3)
         text_ui.add_text(stdscr, align.center_vertical, f'‾ ‾ ‾ ‾', row=1)
-        text_ui.add_text(stdscr, align.center_vertical, f'Back', row=5, color=curses.COLOR_BLACK, background=curses.COLOR_WHITE)
+        text_ui.add_text(stdscr, align.center_vertical, f'Back (S16)', row=5, color=curses.COLOR_BLACK, background=curses.COLOR_WHITE)
         try:
             if entered_keys:
                 pass
@@ -39,16 +83,17 @@ def main_network(stdscr):
         except:
             try_again = False
         #text_ui.add_text(stdscr, align.center_vertical, f'{entered_keys}', row=2)
-        if entered_keys == [50, 56, 49, 50]:
+        if entered_keys == [50, 56, 49, 50] or entered_keys == ["2", "8", "1", "2"]:
             text_ui.add_text(stdscr, align.center_vertical, f'connecting', row=4)
             subprocess.run(["sudo", "systemctl", "start", "parprouted"])
             telegram_send_message("Успешно введен пароль, подключение служб.")
             asyncio_thread = threading.Thread(target=run_check_connection)
             asyncio_thread.start()
+            GPIO.cleanup()
             curses.wrapper(main_schedule)
 
 
-        elif len(entered_keys) == 4 and entered_keys != [50, 56, 49, 50]:
+        elif len(entered_keys) == 4 and entered_keys != [50, 56, 49, 50] or len(entered_keys) == 4 and entered_keys != ["2", "8", "1", "2"]:
             telegram_send_message(f"Неправильная попытка ввода пароля!")
             entered_keys = []
         if len(entered_keys) <= 4:
@@ -58,10 +103,24 @@ def main_network(stdscr):
 
             lable = " ".join(stars)
             text_ui.add_text(stdscr, align.center_vertical, lable, row=0)
+
             key = stdscr.getch()
             if key == ord("1") or key == ord("2") or key == ord("3") or key == ord("4") or key == ord("5") or key == ord("6") or key == ord("7") or key == ord("8") or key == ord("9"):
                 entered_keys.append(key)
             else:
                 if key == KEY_ENTER or key in [10, 13]:
                     curses.wrapper(main_screen)
+            
+            pressed_key = None
+            pressed_key = readLine(L1, ["1", "2", "3", "A"]) or pressed_key
+            pressed_key = readLine(L2, ["4", "5", "6", "B"]) or pressed_key
+            pressed_key = readLine(L3, ["7", "8", "9", "C"]) or pressed_key
+            pressed_key = readLine(L4, ["*", "0", "#", "D"]) or pressed_key
+            if pressed_key is not None:
+                if pressed_key == "D":
+                    GPIO.cleanup()
+                    curses.wrapper(main_screen)
+
+                entered_keys.append(pressed_key)
+
             time.sleep(0.1)
--- screens/schedule/main_screen.py
+++ screens/schedule/main_screen.py
@@ -8,6 +8,7 @@ import time
 import asyncio
 import subprocess
 from pyfiglet import Figlet
+import RPi.GPIO as GPIO
 
 def is_midnight():
     # Получаем текущее время
@@ -64,6 +65,51 @@ def main_schedule(stdscr):
         7: ["13:50", "14:35"],
         8: ["14:45", "15:30"]
     }
+
+    L1 = 5
+    L2 = 6
+    L3 = 13
+    L4 = 19
+
+    C1 = 12
+    C2 = 16
+    C3 = 20
+    C4 = 21
+
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+
+    GPIO.setup(L1, GPIO.OUT)
+    GPIO.setup(L2, GPIO.OUT)
+    GPIO.setup(L3, GPIO.OUT)
+    GPIO.setup(L4, GPIO.OUT)
+
+    GPIO.setup(C1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(C2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(C3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(C4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+    #            ^` ^l      ^o  ^e ^`           ^o  ^a   ^a ^b   ^o     ^o             
+    key_states = {
+        '1': False, '2': False, '3': False, 'A': False,
+        '4': False, '5': False, '6': False, 'B': False,
+        '7': False, '8': False, '9': False, 'C': False,
+        '*': False, '0': False, '#': False, 'D': False
+    }
+    def readLine(line, characters):
+        pressed_key = None
+        GPIO.output(line, GPIO.HIGH)
+        for i, char in enumerate(characters):
+            if GPIO.input([C1, C2, C3, C4][i]) == 1:
+                if not key_states[char]:
+                    print(char)
+                    pressed_key = char
+                    key_states[char] = True
+            else:
+                key_states[char] = False
+        GPIO.output(line, GPIO.LOW)
+        return pressed_key
+
 
     while True:
         stdscr.clear()
@@ -124,7 +170,7 @@ def main_schedule(stdscr):
 
         text_ui.add_text(stdscr, align.left_vertical, "Connection", row=10, color=color1, background=background1)
 
-        line = f"* press q to exit   ||   press n to enter network manager"
+        line = f"* press q (S16) to exit | n (S4) for Network Manager"
         text_ui.add_text(stdscr, align.left_vertical, line, row=11)
         current_time = datetime.now().strftime("%H : %M")
         f = Figlet(font='basic')
@@ -132,7 +178,6 @@ def main_schedule(stdscr):
 
         text_ui.add_text(stdscr, align.left_vertical, line, row=1)
         stdscr.refresh()
-        time.sleep(3)
 
 
         key = stdscr.getch()
@@ -140,3 +185,19 @@ def main_schedule(stdscr):
             curses.wrapper(main_screen)
         if key == ord('n'):
             curses.wrapper(main_network)
+
+        pressed_key = None
+        pressed_key = readLine(L1, ["1", "2", "3", "A"]) or pressed_key
+        pressed_key = readLine(L2, ["4", "5", "6", "B"]) or pressed_key
+        pressed_key = readLine(L3, ["7", "8", "9", "C"]) or pressed_key
+        pressed_key = readLine(L4, ["*", "0", "#", "D"]) or pressed_key
+        if pressed_key is not None:
+            if pressed_key == "A":
+                GPIO.cleanup()
+                curses.wrapper(main_network)
+                curses.wrapper(main_screen)
+            if pressed_key == "D":
+                GPIO.cleanup()
+                curses.wrapper(main_screen)
+
+        time.sleep(0.1)
